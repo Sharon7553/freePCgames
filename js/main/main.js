1fc048da601d639ba789293c59311b32a529bd1e
Sharon7553 <sharonp7553@hotmail.com>
2024-11-08 01:00:28 +0000

    revert
    .

--- js/main/main.js
+++ js/main/main.js
@@ -8,20 +8,4 @@ function revealCode() {
     } else {
         codeText.style.filter = 'blur(6px)'; // Blur the code to hide it
     }
-}
 
-// Script to fade the background as the user scrolls
-window.addEventListener('scroll', function() {
-  var scrollPosition = window.scrollY;
-  var background = document.querySelector('.background');
-
-  // Calculate opacity based on scroll position (you can adjust the value as needed)
-  var opacityValue = 1 - (scrollPosition / 500);  // 500 is the scroll distance after which the image fades completely
-
-  // Prevent the opacity from going below 0
-  if (opacityValue < 0) {
-    opacityValue = 0;
-  }
-
-  background.style.opacity = opacityValue;
-});
