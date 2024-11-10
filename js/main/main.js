9e8699b6543bfcc9cfa1f66f79e629dccd51cea5
Sharon7553 <sharonp7553@hotmail.com>
2024-11-10 12:14:36 +0000

    added membership

--- js/main/main.js
+++ js/main/main.js
@@ -25,6 +25,36 @@ document.addEventListener('copy', function(e) {
     alert('Copying is disabled on this page.');
 });
 
+// Import Supabase library if it's not already in your HTML
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js';
+
+// Supabase initialization
+const supabaseUrl = 'https://knlovzpoxlozjgkgttou.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtubG92enBveGxvempna2d0dG91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyMzc0MzcsImV4cCI6MjA0NjgxMzQzN30.xiw4xK4TqOTp-c5VG8GbequxIYJQbfuUIEsPajig7xs';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+$(function(){
+  $('.hide-show').show();
+  $('.hide-show span').addClass('show')
+  
+  $('.hide-show span').click(function(){
+    if( $(this).hasClass('show') ) {
+      $(this).text('Hide');
+      $('input[name="login[password]"]').attr('type','text');
+      $(this).removeClass('show');
+    } else {
+       $(this).text('Show');
+       $('input[name="login[password]"]').attr('type','password');
+       $(this).addClass('show');
+    }
+  });
+    
+    $('form button[type="submit"]').on('click', function(){
+        $('.hide-show span').text('Show').addClass('show');
+        $('.hide-show').parent().find('input[name="login[password]"]').attr('type','password');
+    }); 
+});
+
 
 
 
